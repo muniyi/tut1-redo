9468aa6283bfd88363a5cd1763aad9d7b2ec1127
muniyi <140512338+muniyi@users.noreply.github.com>
2023-07-26 22:12:50 +1200

    Added feature 5

--- Greeter.java
+++ Greeter.java
@@ -1,14 +1,18 @@
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.time.LocalDate;
+
 
 public class Greeter {
 
   private Locale locale;
   private String name;
+  private LocalDate birthday;
 
-  public Greeter(String language, String country, String name) {
+  public Greeter(String language, String country, String name, LocalDate birthday) {
     locale = new Locale(language, country);
     this.name = name;
+    this.birthday = birthday;
   }
 
   public String sayHello() {
@@ -16,11 +20,20 @@ public class Greeter {
     return messages.getString("greeting") + ", " + "<strong>" + name.toUpperCase() + "</strong>";
   }
 
+  public boolean isBirthday() {
+    LocalDate today = LocalDate.now();
+    return today.getMonth() == birthday.getMonth() && today.getDayOfMonth() == birthday.getDayOfMonth();
+  }
+
   public static void main(String[] args) {
     String language = args[0];
     String country = args[1];
     String name = args[2] + args[3];
-    Greeter greeter = new Greeter(language, country, name);
+    LocalDate birthday = LocalDate.parse(args[5]);
+    Greeter greeter = new Greeter(language, country, name, birthday);
     System.out.println(greeter.sayHello());
+    if (greeter.isBirthday()) {
+       System.out.println("Happy Birthday!");
+    }
   }
 }
