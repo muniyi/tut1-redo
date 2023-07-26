70ff92d91a7e36942e8510b36310497b2790565f
muniyi <140512338+muniyi@users.noreply.github.com>
2023-07-26 21:57:20 +1200

    Added feature-4

--- Greeter.java
+++ Greeter.java
@@ -5,22 +5,25 @@ public class Greeter {
 
   private Locale locale;
   private String name;
+  private int userid;
 
-  public Greeter(String language, String country, String name) {
+  public Greeter(String language, String country, String name, int userid) {
     locale = new Locale(language, country);
     this.name = name;
+    this.userid = userid;
   }
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + "<strong>" + name.toUpperCase() + "</strong>";
+    return messages.getString("greeting") + ", " + "<strong>" + name.toUpperCase() + "</strong>" + " #" + String.valueOf(userid);
   }
 
   public static void main(String[] args) {
     String language = args[0];
     String country = args[1];
     String name = args[2] + args[3];
-    Greeter greeter = new Greeter(language, country, name);
+    int userid = Integer.parseInt(args[4]);
+    Greeter greeter = new Greeter(language, country, name, userid);
     System.out.println(greeter.sayHello());
   }
 }
